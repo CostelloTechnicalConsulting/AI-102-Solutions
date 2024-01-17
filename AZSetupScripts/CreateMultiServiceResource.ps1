cab6231ca163312970d51d0bfa100d5963a8afd9
Dan <dan@costellotech.com>
2024-01-17 14:40:56 -0500

    Added feature to retrieve keys and endpoint

--- AZSetupScripts/CreateMultiServiceResource.ps1
+++ AZSetupScripts/CreateMultiServiceResource.ps1
@@ -10,3 +10,8 @@ az login
 #az cognitiveservices account list-kinds
 
 az cognitiveservices account create --name $azureAIServicesName --resource-group $resourceGroupName  --kind CognitiveServices --sku S0 --location $location --yes
+
+# get keys and endpoint for Azure AI Services
+$azureAIServicesKey = az cognitiveservices account keys list --name $azureAIServicesName --resource-group $resourceGroupName --query key1 -o tsv
+$azureAIServicesEndpoint = az cognitiveservices account show --name $azureAIServicesName --resource-group $resourceGroupName --query endpoint -o tsv
+
